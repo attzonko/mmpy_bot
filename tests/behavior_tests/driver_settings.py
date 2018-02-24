f5a8f93787f4144bf801f90b1560ca4e5fb9aabd
seLain <selain@nature.ee.ncku.edu.tw>
2018-02-24 08:46:05 +0800

    add tests

---
+++ tests/behavior_tests/driver_settings.py
@@ -0,0 +1,11 @@
+PLUGINS = [
+]
+
+BOT_URL = 'http://SERVER_HOST_DN/api/v4'
+BOT_LOGIN = 'driverbot@mail'
+BOT_NAME = 'driverbot_name'
+BOT_PASSWORD = 'driverbot_password'
+BOT_TEAM = 'default_team_name'	# this team name should be the same as in bot_settings
+BOT_CHANNEL = 'off-topic' # default public channel name
+BOT_PRIVATE_CHANNEL = 'test' # a private channel in BOT_TEAM
+SSL_VERIFY = True
---
+++ tests/unit_tests/single_plugin/mock_plugin.py
@@ -0,0 +1,9 @@
+from mattermost_bot.utils import allow_only_direct_message
+from mattermost_bot.utils import allowed_users
+from mattermost_bot.bot import respond_to
+
+@respond_to('^admin$')
+@allow_only_direct_message()
+@allowed_users('admin', 'root')
+def mock_users_access(message):
+    message.reply('Access allowed!')
