7849600bc1ba652eaef3ec592386ef245234f979
Victor Hu <selain@nature.ee.ncku.edu.tw>
2018-07-17 13:36:02 +0800

    Enable bot to ignore messages from specified senders (#29)
    * fix incorrect test
    
    If there are two plugin functions listening to the same regex pattern, only the latest funciton will be loaded in plugin manager. Details can be found in bot.listen_to(). So there sould not be test for loading two plugin functions listening to the same regex pattern.
    
    * test for 1 to 1 function-regexp mapping
    
    append test to ensure the latest loaded function replaces previous loaded function which listening to the same regexp statement.
    
    * unit test for ignoring senders specified in settings.IGNORE_USERS
    
    * enable dispatcher to ignore messages from specified senders
    
    * move get_sender as static method

---
+++ tests/unit_tests/test_ignoremessage.py
@@ -0,0 +1,21 @@
+import logging
+from mmpy_bot.dispatcher import MessageDispatcher
+from mmpy_bot import settings
+
+#logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+def test_messagedispatcher__ignore_sender():
+	dispatcher = MessageDispatcher(None, None)
+	event = {'event': 'posted', 'data': {'sender_name': 'betty'}}
+	event2 = {'event': 'posted', 'data': {'sender_name': 'Carter'}}
+	# test by lowercase  / uppercase settings
+	# set as 'Betty'
+	settings.IGNORE_USERS = [] # clean up
+	settings.IGNORE_USERS.append('Betty')
+	if dispatcher._ignore_sender(event) is not True:
+		raise AssertionError()
+	# set as 'CARTER'
+	settings.IGNORE_USERS.append('CARTER')
+	if dispatcher._ignore_sender(event2) is not True:
+		raise AssertionError()
