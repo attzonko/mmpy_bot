4ea98ef9e924f3796ea3f144c93ba7ac5eb575f9
tgly307 <tgly307@163.com>
2018-08-05 15:35:08 +0800

    add driver info to mock_plugin.py

--- tests/unit_tests/single_plugin/mock_plugin.py
+++ tests/unit_tests/single_plugin/mock_plugin.py
@@ -1,10 +1,10 @@
 from mmpy_bot.utils import allow_only_direct_message
 from mmpy_bot.utils import allowed_users
 from mmpy_bot.bot import respond_to
-
+from tests.behavior_tests.driver_settings import BOT_NAME, BOT_LOGIN
 
 @respond_to('^admin$')
 @allow_only_direct_message()
-@allowed_users('admin', 'root')
+@allowed_users(BOT_NAME, BOT_LOGIN)
 def mock_users_access(message):
     message.reply('Access allowed!')
