069f810019159fe17c18735d29425478d3de1676
freemagico <tgly307@163.com>
2018-08-16 10:50:02 +0800

    roll back test

--- tests/unit_tests/single_plugin/mock_plugin.py
+++ tests/unit_tests/single_plugin/mock_plugin.py
@@ -7,14 +7,3 @@ from mmpy_bot.bot import respond_to
 @allowed_users('admin', 'root')
 def mock_users_access(message):
     message.reply('Access allowed!')
-
-
-@respond_to('file$')
-def get_timestamp_file(message):
-    file = open('new.txt', 'w+')
-    result = message.upload_file(file)
-    file.close()
-    if 'file_infos' not in result:
-        message.reply('upload file error')
-    file_id = result['file_infos'][0]['id']
-    message.reply('hello', [file_id])
