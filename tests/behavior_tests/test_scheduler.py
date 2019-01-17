d5ea98bd215b3f81d6b84c39f6649fd993a8b480
Alex Tzonkov <alex.tzonkov@gmail.com>
2019-01-16 22:01:36 -0800

    Increase wait time for CI
    Seems that on Travis CI we are timing out before seeing the message
    back from the responder bot. Adding 5 more seconds to see if the issue
    will be fixed.

--- tests/behavior_tests/test_scheduler.py
+++ tests/behavior_tests/test_scheduler.py
@@ -10,15 +10,15 @@ def test_bot_reply_specific_time(driver):
     driver.send_direct_message(
         'reply "{0}" at {0}'.format(str_t_time), tobot=True)
     driver.wait_for_bot_direct_message(
-        str_t_time, maxwait=settings.JOB_TRIGGER_PERIOD+5)
+        str_t_time, maxwait=settings.JOB_TRIGGER_PERIOD+10)
 
 
 def test_bot_reply_every_seconds(driver):
     driver.send_direct_message('reply "alive" every 6 seconds', tobot=True)
     driver.wait_for_bot_direct_message(
-        'alive', maxwait=settings.JOB_TRIGGER_PERIOD+5)
+        'alive', maxwait=settings.JOB_TRIGGER_PERIOD+10)
     driver.wait_for_bot_direct_message(
-        'alive', maxwait=settings.JOB_TRIGGER_PERIOD+5)
+        'alive', maxwait=settings.JOB_TRIGGER_PERIOD+10)
     driver.send_direct_message('cancel jobs', tobot=True)
     driver.wait_for_bot_direct_message(
-        'all jobs canceled.', maxwait=settings.JOB_TRIGGER_PERIOD+5)
+        'all jobs canceled.', maxwait=settings.JOB_TRIGGER_PERIOD+10)
