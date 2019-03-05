29e909b93e1122cf3e9e389f26335beb16fdd5d6
Pierre Verkest <pverkest@anybox.fr>
2019-03-05 18:22:07 +0100

    reply_thread add message in existing thread if present (#104)
    * reply_thread add message in existing thread if present
    
    * reply_thread: install mock lib for python 2.7 support

--- tests/unit_tests/test_message.py
+++ tests/unit_tests/test_message.py
@@ -1,6 +1,10 @@
 import os
 import json
 import pytest
+try:
+    from unittest import mock
+except ImportError:
+    from mock import mock
 from mmpy_bot.dispatcher import Message
 
 
@@ -8,6 +12,14 @@ from mmpy_bot.dispatcher import Message
 def message():
     with open(os.sep.join(
               ['tests', 'unit_tests', 'test_data', 'message.json']),
+              'r') as f:
+        return Message(client=None, body=json.load(f), pool=None)
+
+
+@pytest.fixture(scope="function")
+def thread_message():
+    with open(os.sep.join(
+              ['tests', 'unit_tests', 'test_data', 'thread_message.json']),
               'r') as f:
         return Message(client=None, body=json.load(f), pool=None)
 
@@ -44,3 +56,14 @@ def test_body(message):
         body = json.load(f)
         if message.body != body:
             raise AssertionError()
+
+
+@mock.patch("mmpy_bot.dispatcher.Message.send")
+def test_first_reply_in_thread(mo_send, thread_message):
+    thread_message.reply_thread("third message in thread")
+    mo_send.assert_called_once_with(
+        "@betty: third message in thread",
+        files=None,
+        props={},
+        pid='wqpuawcw3iym3pq63s5xi1776r'
+    )
