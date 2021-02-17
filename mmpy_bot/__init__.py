ac5aa9af35f947e16eeaae935a821fe2969b637b
Alex Tzonkov <4975715+attzonko@users.noreply.github.com>
2021-02-17 19:31:37 +0000

    Incrementing version for release

--- mmpy_bot/__init__.py
+++ mmpy_bot/__init__.py
@@ -1,5 +1,8 @@
-VERSION = (1, 3, 4)
+"""A python based chat bot for [Mattermost](http://www.mattermost.org)."""
+VERSION = (1, 3, 5)
 
 
 def get_version():
     return '.'.join(map(str, VERSION))
+
+__version__ = get_version()
