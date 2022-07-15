a9bdb63ad2aeab202fce1d217250bca44a6608cb
Renato Alves <alves.rjc@gmail.com>
2022-07-15 22:19:16 +0200

    Remove help functions superseeded by HelpPlugin

--- mmpy_bot/plugins/base.py
+++ mmpy_bot/plugins/base.py
@@ -8,10 +8,10 @@ from dataclasses import dataclass
 from typing import Any, Dict, List, MutableSequence, Optional, Sequence, Union
 
 from mmpy_bot.driver import Driver
-from mmpy_bot.function import Function, MessageFunction, WebHookFunction, listen_to
+from mmpy_bot.function import Function, MessageFunction, WebHookFunction
 from mmpy_bot.settings import Settings
 from mmpy_bot.utils import split_docstring
-from mmpy_bot.wrappers import EventWrapper, Message
+from mmpy_bot.wrappers import EventWrapper
 
 log = logging.getLogger("mmpy.plugin_base")
 
@@ -33,11 +33,6 @@ class Plugin(ABC):
         self.webhook_listeners: Dict[
             re.Pattern, MutableSequence[WebHookFunction]
         ] = defaultdict(list)
-
-        # We have to register the help function listeners at runtime to prevent the
-        # Function object from being shared across different Plugins.
-        self.help = listen_to("^help$", needs_mention=True)(Plugin.help)
-        self.help = listen_to("^!help$")(self.help)
 
     def initialize(self, driver: Driver, settings: Optional[Settings] = None):
         self.driver = driver
@@ -89,25 +84,6 @@ class Plugin(ABC):
             # By default, we use the global threadpool of the driver, but we could use
             # a plugin-specific thread or process pool if we wanted.
             self.driver.threadpool.add_task(function, event, *groups)
-
-    def get_help_string(self):
-        string = f"Plugin {self.__class__.__name__} has the following functions:\n"
-        string += "----\n"
-        for functions in self.message_listeners.values():
-            for function in functions:
-                string += f"- {function.get_help_string()}"
-            string += "----\n"
-        if len(self.webhook_listeners) > 0:
-            string += "### Registered webhooks:\n"
-            for functions in self.webhook_listeners.values():
-                for function in functions:
-                    string += f"- {function.get_help_string()}"
-
-        return string
-
-    async def help(self, message: Message):
-        """Prints the list of functions registered on every active plugin."""
-        self.driver.reply_to(message, self.get_help_string())
 
 
 @dataclass
