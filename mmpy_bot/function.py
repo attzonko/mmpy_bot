035c3ae91a775e6c0a6347d2973b72b32094efe1
ArtemIsmagilov <artem.ismagilov.2000@mail.ru>
2024-07-16 12:57:17 +0400

    remove deprecated `asyncio.iscoroutinefunction` and use `inspect.iscoroutinefunction`

--- mmpy_bot/function.py
+++ mmpy_bot/function.py
@@ -43,7 +43,7 @@ class Function(ABC):
             )
 
         self.function = function
-        self.is_coroutine = asyncio.iscoroutinefunction(function)
+        self.is_coroutine = inspect.iscoroutinefunction(function)
         self.is_click_function: bool = False
         self.matcher = matcher
         self.metadata = metadata
@@ -100,7 +100,7 @@ class MessageFunction(Function):
 
         if self.is_click_function:
             _function = self.function.callback
-            if asyncio.iscoroutinefunction(_function):
+            if inspect.iscoroutinefunction(_function):
                 raise ValueError(
                     "Combining click functions and coroutines is currently not supported!"
                     " Consider using a regular function, which will be threaded by default."
